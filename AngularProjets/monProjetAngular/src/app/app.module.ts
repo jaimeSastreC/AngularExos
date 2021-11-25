37bc5690b6a371500ea179b550cda12728a579c8
jaimeSastreC <jaime.sastre@lapiscine.pro>
2021-11-25 17:47:58 +0100

    component chapt 9 c09

--- AngularProjets/monProjetAngular/src/app/app.module.ts
+++ AngularProjets/monProjetAngular/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MpcComponent } from './composants/mpc/mpc.component';
+import { C09Component } from './composants/c09/c09.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MpcComponent
+    MpcComponent,
+    C09Component
   ],
   imports: [
     BrowserModule,

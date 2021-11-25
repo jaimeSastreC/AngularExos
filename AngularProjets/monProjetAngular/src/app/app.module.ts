ecd64e5332004301705c78cf63a5e87796ba37ab
jaimeSastreC <jaime.sastre@lapiscine.pro>
2021-11-25 16:23:40 +0100

    installation Bootstrap et jquery

--- AngularProjets/monProjetAngular/src/app/app.module.ts
+++ AngularProjets/monProjetAngular/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MpcComponent } from './composants/mpc/mpc.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MpcComponent
   ],
   imports: [
     BrowserModule,

10c679a62c38e9710f07c8c47cc86a71937f709c
jaimeSastreC <jaime.sastre@lapiscine.pro>
2021-11-27 00:18:02 +0100

    ex c09 sauvegarde intermédiaire fin de journée

--- AngularProjets/monProjetAngular/src/app/composants/c09/c09.component.ts
+++ AngularProjets/monProjetAngular/src/app/composants/c09/c09.component.ts
@@ -7,9 +7,54 @@ import { Component, OnInit } from '@angular/core';
 })
 export class C09Component implements OnInit {
 
+  unString = 'Un texte à afficher!';
+  unChiffre = 8008;
+  unObjet = {nom: 'Wayne', prenom: 'Bruce', ville: 'Gotham City'};
+
+sBouton = "Pas encore cliqué";
+nbClic = 0;
+
+sportifs = ['Laurence Cousin', 'Bruce Lee', 'Roger Gracie', 'Ryu'];
+
+maClasse = 'alert-primary';
+unBooleen = false;
+unObjetClass = {
+  'badge': false,
+  'badge-pill': true,
+  'badge-primary': true
+  };
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  unClic() {
+    console.log("Cliqué");
+    this.nbClic++;
+    this.sBouton = 'Cliqué ' + this.nbClic + " de fois";
+  }
+
+  changeClass() {
+    if (this.maClasse === 'alert-success'){
+      this.maClasse = 'alert-danger';
+      console.log(this.maClasse);
+    } else {
+      this.maClasse = 'alert-success';
+      console.log(this.maClasse)
+    }
+  }
+
+  changeClassBoolean() {
+    this.unBooleen = !this.unBooleen;
+  }
+
+  changeClassObjet(){
+    if(this.unObjetClass.badge === true){
+      this.unObjetClass.badge = false;
+    } else {
+      this.unObjetClass.badge = true;
+    }
   }
 
 }
